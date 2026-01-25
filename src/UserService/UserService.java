c379cc9134a5bd00fc7737f7c67df973237b6327
Daniel Steven <dan.p.steven@gmail.com>
2026-01-25 16:21:10 -0500

    add UserHandler() for UserService

--- src/UserService/UserService.java
+++ src/UserService/UserService.java
@@ -2,14 +2,30 @@ package UserService;
 
 import java.io.IOException;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
 import Shared.MicroService;
 
 
 public class UserService extends MicroService{
 
+    private String context = "/user";
 
     public UserService (String ip, int port) throws IOException{
+
         super(ip, port);
+        addContext(context, new UserHandler());
+
+
+    }
+
+    static class UserHandler implements HttpHandler {
+        @Override
+        public void handle(HttpExchange exchange) throws IOException {
+            String method = exchange.getRequestMethod();
+            System.out.println(method);
+        }
     }
 
     public static void main(String[] args) throws IOException{
@@ -19,7 +35,7 @@ public class UserService extends MicroService{
 
         UserService u = new UserService(ip, port);
         u.start();
-        u.stop(5);
+        //u.stop(5);
     }
 
     //User updateUser(User user, int id, String email, String username, String password) {
