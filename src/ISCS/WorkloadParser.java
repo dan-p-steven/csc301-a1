9c55d172333d96eab4b177a66cf7c5b1c84a6d49
Simon Hunter <72155251+Nomis1123@users.noreply.github.com>
2026-01-26 13:57:11 -0500

    created workloadparser file

---
+++ src/ISCS/WorkloadParser.java
@@ -0,0 +1,15 @@
+package ISCS;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+public class WorkloadParser {
+
+    private static final String ORDER_SERVICE_URL = "http://127.0.0.1:14000";
+    
+}
