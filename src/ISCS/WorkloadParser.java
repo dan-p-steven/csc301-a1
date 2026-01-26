ee087001163a97bbb7bd971f33b23464faadf618
Simon Hunter <72155251+Nomis1123@users.noreply.github.com>
2026-01-26 14:00:20 -0500

    added main WorkloadParser

--- src/ISCS/WorkloadParser.java
+++ src/ISCS/WorkloadParser.java
@@ -11,5 +11,17 @@ import java.util.Scanner;
 public class WorkloadParser {
 
     private static final String ORDER_SERVICE_URL = "http://127.0.0.1:14000";
+
+    public static void main(String[] args)
+    {
+        if (args.length < 1)
+        {
+            System.out.println("Usage: java WorkloadParser <workload_file_path>");
+        }
+
+        String filename = args[0];
+        ProcessWorkloadFile(filename);
+    }
     
 }
+
