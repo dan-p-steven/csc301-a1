8c024ed107560db06e7e820ef751f5425eb08185
Simon Hunter <72155251+Nomis1123@users.noreply.github.com>
2026-01-28 11:10:12 -0500

    worked on forwardHandler

--- src/ISCS/ISCS.java
+++ src/ISCS/ISCS.java
@@ -14,14 +14,45 @@ import java.lang.reflect.Type;
 import java.util.Map;
 
 
-public class ISCS extends MicroService {
-
+public class ISCS extends MicroService 
+{
     private static final String SERVICE_NAME = "InterServiceCommuncation";
     private static final Gson gson = new Gson();
 
     private final ServerConfig userServiceConfig;
     pricvate final ServerConfig productServiceConfig;
 
-    
-    
+    public ISCS(String ip, int port, ServerConfig userConfig, ServerConfig productConfig) throws IOException 
+    {
+        super(ip, port);
+        this.userServiceConfig = userConfig;
+        this.productServiceConfig = productConfig;
+
+        // Route /user requests to the UserService
+        addContext("/user", new ForwardHandler(userServiceConfig));
+        
+        // Route /product requests to the ProductService
+        addContext("/product", new ForwardHandler(productServiceConfig));
+    }
+
+    static class ForwardHandler implements HttpHandler
+    {
+        private final ServerConfig targetConfig;
+
+        public ForwardHandler(ServerConfig targetConfig) 
+        {
+            this.targetConfig = targetConfig;
+        }
+
+        @Override
+        public void handle(HttpExchange exchange) throws IOException
+        {
+            String path = exchange.getRequestURI().toString();
+            String targetUrlString = "http://" + targetConfig.ip + ":" + targetConfig.port + path;
+
+            System.out.println("[ISCS] Forwarding request to: " + exchange.getRequestMethod() + targetUrlString);
+        }
+    }
+
 }
+
